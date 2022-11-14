29815e2d2e28feca0880f2afcea67d965b9e2b12
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-14 21:19:32 +0300

    homepage styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import "./styles/main.scss";
+
 function App() {
   return (
     <>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
+        <Route
+          path="/"
+          element={
+            <AnimatePresence>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7 }}
+                exit={{ x: -1500, opacity: 0 }}
+              >
+                <Home></Home>
+              </motion.div>
+            </AnimatePresence>
+          }
+        ></Route>
         <Route path="/products" element={<Products></Products>}>
           <Route path=":id" element={<Products></Products>}></Route>
         </Route>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,36 @@
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <h1>HOME</h1>;
+  const navigate = useNavigate();
+  const navigateProductsPage = () => {
+    navigate("/products");
+  };
+  return (
+    <main className="home">
+      <motion.h1
+        className="home-title"
+        initial={{ y: -500 }}
+        animate={{ y: [-1500, 25, 0] }}
+        transition={{
+          duration: 1.2,
+          ease: "easeOut",
+        }}
+      >
+        Sevdem Will Brighten Your Day
+      </motion.h1>
+      <motion.button
+        className="order-button"
+        initial={{ y: 1500 }}
+        animate={{ y: [2500, 0] }}
+        transition={{
+          duration: 0.4,
+          ease: "easeOut",
+        }}
+        onClick={navigateProductsPage}
+      >
+        Order Now
+      </motion.button>
+    </main>
+  );
 }
